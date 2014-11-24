e9ae78cef36dd0f38ba4b315005def55ded4aa4c
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-11-24 19:43:49 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+#
+
+> Zuul plays nice with Browserify
+
+# Install
+
+From npm.
+
+```shell
+npm install zuulnice
+```
+
+# Use
+
+Configure Zuul using the `.zuul.yml` yaml file. First off, add the `zuulnice` builder.
+
+```yaml
+builder: zuulnice
+```
+
+It works exactly like the default builder, and it adds extra functionality through the yaml configuration. You can set any of the configuration elements in the list below.
+
+- `plugin`
+- `external`
+- `ignore`
+- `exclude`
+- `transform`
+- `add`
+- `require`
+
+The syntax is as follows. You can pass options to any of them!
+
+```yaml
+builder: zuulnice
+browserify:
+  - plugin: proxyquire-universal
+  - require: ./some-file.js
+    expose: intimidate
+    entry: true
+  - external: ./some-module.js
+  - transform: brfs
+```
+
+# License
+
+MIT
---
+++ zuulnice.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+var browserify = require('browserify');
+var convert = require('convert-source-map');
+var yaml = require('yamljs');
+var file = path.join(process.cwd(), '.zuul.yml');
+var config = fs.existsSync(file) ? yaml.parse(read(file)) : {};
+var bro = config.browserify;
+
+function read (file) {
+  return fs.readFileSync(file, 'utf8');
+}
+
+function configure (bundler) {
+  ['plugin', 'external', 'ignore', 'exclude', 'transform', 'add', 'require'].forEach(registerable);
+
+  function registerable (type) {
+    _.where(bro, type).forEach(register.bind(null, type));
+  }
+
+  function register (type, o) {
+    bundler[type](o[type], _.omit(o, type));
+  }
+}
+
+module.exports = function(files, opt, cb) {
+  if (typeof opt === 'function') {
+    cb = opt;
+    opt = {};
+  }
+  if (!opt) {
+    opts = {};
+  }
+  opt.debug = true;
+
+  var bundler = browserify(opt);
+
+  if (bro) {
+    configure(bundler);
+  }
+
+  files.forEach(function(file) {
+    bundler.require(file, { entry: true });
+  });
+
+  bundler.bundle(function(err, buf) {
+    if (err) {
+      return cb(err);
+    }
+
+    var src = buf.toString();
+    var srcmap = convert.fromSource(src);
+    var map = undefined;
+    src = convert.removeComments(src);
+
+    if (srcmap) {
+      map = srcmap.toObject();
+    }
+
+    cb(null, src, map);
+  });
+};
